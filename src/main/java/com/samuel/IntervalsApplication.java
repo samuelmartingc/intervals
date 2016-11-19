24d3fe78bc336cca6e48e9d84bda63a54ae6bf5f
samuelmartingc <samuelmartingc@gmail.com>
2016-11-19 13:18:29 +0100

    first commit

---
+++ src/main/java/com/samuel/IntervalsApplication.java
@@ -0,0 +1,12 @@
+package com.samuel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntervalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntervalsApplication.class, args);
+	}
+}
